b895ac3a1ce7460c32bfcc853c71776bc8b7e275
helozen <shaktishihab247#gmail.com>
2025-01-17 03:56:24 +0600

    Refactor Firebase configuration and update signup page to use module scripts

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
-// Firebase Configuration
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID",
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
