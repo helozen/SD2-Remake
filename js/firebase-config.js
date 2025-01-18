7a92cded36dcca79f10b822f6a372edbf65016c3
helozen <shaktishihab247#gmail.com>
2025-01-18 15:30:40 +0600

    Update Firebase configuration and add signup form with user type selection

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,21 @@
-// firebase-config.js
+<script type="module">
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAiUw7hCOjJwJm5RJRfxV7iJ9gNjNLtfkI",
+  authDomain: "sd-2-66867.firebaseapp.com",
+  projectId: "sd-2-66867",
+  storageBucket: "sd-2-66867.firebasestorage.app",
+  messagingSenderId: "910745957167",
+  appId: "1:910745957167:web:f454cebdff8ffff7c727da",
+  measurementId: "G-M7X7XNRBW1",
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+</script>
